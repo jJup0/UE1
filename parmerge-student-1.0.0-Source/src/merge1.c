b70d3ded2b6cf4f3e77205f10b6eb526c6768ad6
Jakob R <nothanks@email.com>
2022-05-23 17:05:47 +0200

    formatting changes for merge1

--- parmerge-student-1.0.0-Source/src/merge1.c
+++ parmerge-student-1.0.0-Source/src/merge1.c
@@ -5,47 +5,48 @@
  *      Author: Jakob
  */
 
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 
 #include "merge.h"
 
-int rank ( double val , double arr [] , int n ){
-  int lo = 0;
-  int hi = n-1;
-  while (lo <= hi){
-    int mid = (lo + hi) >> 1;
-    if (arr[mid] < val){
-      lo = mid + 1;
-    } else {
-      hi = mid - 1;
+int rank(double val, double arr[], int n) {
+    int lo = 0;
+    int hi = n - 1;
+    while (lo <= hi) {
+        int mid = (lo + hi) >> 1;
+        if (arr[mid] < val) {
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
     }
-  }
-  return lo;
+    return lo;
 }
 
 void merge(double a[], long n, double b[], long m, double c[]) {
 #pragma omp parallel
-  {
-    int i;
-    int j;
+    {
+        int i;
+        int j;
 
-    // rank all elements in a, and insert them into c
+        // rank all elements in a, and insert them into c
 #pragma omp for nowait
-    for (i=0; i<n; i++) {
-      int rank_a = rank(a[i], b, m);
-      c[i+rank_a] = a[i];
-    }
+        for (i = 0; i < n; i++) {
+            c[i + rank(a[i], b, m)] = a[i];
+        }
 
-    // rank all elements in b, and insert them into c
+        // rank all elements in b, and insert them into c
 #pragma omp for nowait
-    for (j=0; j<m; j++) {
-      int rank_b = rank(b[j], a, n);
-      c[j+rank_b] = b[j];
+        for (j = 0; j < m; j++) {
+            c[j + rank(b[j], a, n)] = b[j];
+        }
     }
-  }
 }
 /*
 ./bin/merge1_tester -n 10 -m 8 -p 2 -c
+./bin/merge1_tester -n 10000000 -m 20000000 -p 2 -c
+./bin/merge1_tester -n 100000000 -m 200000000 -p 2 -c
+./bin/merge1_tester -n 100000000 -m 200000000 -s
 */
