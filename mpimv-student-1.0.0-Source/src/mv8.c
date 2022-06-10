0ac38c6a30d2b94eba1b7cadd9a79bd9505b4b03
Berik-Xardas <94537393+Berik-Xardas@users.noreply.github.com>
2022-06-10 23:06:56 +0200

    Updated 8

--- mpimv-student-1.0.0-Source/src/mv8.c
+++ mpimv-student-1.0.0-Source/src/mv8.c
@@ -1,56 +1,75 @@
 #include <mpi.h>
 
+/*
+ A: Pointer to the input matrix (only the Submatrix the process has access to)
+ nrows: number of rows of A
+ ncols: number of columns of A
+ nrows_a_loc: how many rows the current process received
+ ncols_a_loc: how many columns (per row) the current process received (=ncols)
+ x: Pointer to the columns of the input vector (where the process starts)
+ ncols_x_loc: how many entries of the input vector the process receives
+ b: pointer to the parts of the result vector the process writes on
+ nrows_b_loc: how many entries of b the process writes on
+*/
 void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc,
         base_t *x, int ncols_x_loc,
-        base_t *b, int nrows_b_loc){
+        base_t *b, int nrows_b_loc)
+{
 
-            int rank, size;
-            MPI_Comm_size(MPI_COMM_WORLD,&size);
-            MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-            
-            int chonk = nrows/size;
-            if (rank == size-1){
-                chonk += nrows%size;
-            }
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-            MPI_DOUBLE* sendbuf = malloc(ncols * sizeof(MPI_DOUBLE)) //sendbuf is "result"
+    MPI_DOUBLE[ncols_x_loc] sendbuf = x;
 
-            int[size] recvcounts;
-            for(int i = 0; i < size; i++){
-                recvcounts[i] = chonk;
-            }// we have size processes - recvcounts[0] indicates the number of data process 0 receives
-            
-            int[size] displs; // we have size processes - recvcounts[0] indicates the start from where process 0 receives data from sendbuf
-            for(int i = 0; i < size; i++){
-                displs[i] = i * chonk;
-            }
+    /*
+    int chonk = nrows/size;
+    if (rank == size-1){
+        chonk += nrows % size;
+    }
 
-            //error handling?
-            MPI_Allgatherv(sendbuf, chonk, MPI_DOUBLE,
-            b, recvcounts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
+    int[size] recvcounts;
+    for(int i = 0; i < size; i++){
+        recvcounts[i] = chonk;
+    }
 
-            for(int i = 0; i < chonk; i++){
-                b[i] = 0;
-                for(int j = 0; j < ncols; i++){
-                    b[i] += A[i][j]*x[j];
-                }
-            }
-            free(sendbuf);
+    int[size] displs;
+    for(int i = 0; i < size; i++){
+        displs[i] = i * chonk;
+    }
+    */
+
+    int[size] displs;
+    displs[rank] = ncols_x_loc;
+    MPI_Allgather(displs, 1, int, displs, 1, int, MPI_COMM_WORLD);
+
+    int[site] recvcounts;
+    if (rank == size - 1)
+    {
+        recvcounts[rank] = ncols - displs[rank];
+    }
+    else
+    {
+        recvcounts[rank] = displs[rank + 1] - displs[rank];
+    }
+    MPI_Allgather(recvcounts, 1, int, recvcounts, 1, int, MPI_COMM_WORLD);
+
+    // error handling?
+    MPI_Allgatherv(sendbuf, recvcounts[rank], MPI_DOUBLE,
+                   x, recvcounts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    for (int i = ncols_x_loc; i < recvcounts[rank]; i++)
+    {
+        b[i] = 0;
+        for (int j = 0; j < ncols; i++)
+        {
+            b[i] += A[i][j] * x[j];
         }
+    }
+    free(sendbuf);
+}
 
 int main(int argc, char[][] argv)
 {
-
-    //TODO: proper argument parsing
-    MPI_DOUBLE **A = argv[1];
-    MPI_DOUBLE *x = argv[2];
-    MPI_DOUBLE *b = malloc(ncols * sizeof(MPI_DOUBLE));
-
-    //error handling?
-    MPI_Init(&argc, &argv);
-    mv(A,nrows,ncols,nrows_a_loc,ncols_a_loc,x,ncols_x_loc,b,nrows_b_loc);
-
-    MPI_Finalize();
-    fprint(&b);
-    free(b);
+    // sir, please calm down and stay where you are - your local FBI agent is on his way to you
 }
