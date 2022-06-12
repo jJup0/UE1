97c07e8402133884bca4252c74270a5735e6c01b
Jakob R <nothanks@email.com>
2022-06-12 21:26:40 +0200

    added free to mv2

--- mpimv-student-1.0.0-Source/src/mv2.c
+++ mpimv-student-1.0.0-Source/src/mv2.c
@@ -1,33 +1,24 @@
+#include <assert.h>
+#include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <assert.h>
-#include <math.h>
-
-#include <mpi.h>
-
-#include "utils.h"
 #include "mv.h"
+#include "utils.h"
 
-void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc,
-        base_t *x, int nrows_x_loc,
-        base_t *b, int ncols_b_loc)
-{
-
+void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc, base_t *x, int nrows_x_loc, base_t *b, int ncols_b_loc) {
     int rank, size;
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     base_t *localB;
     alloc_vector(&localB, ncols);
 
-
-    for (int i = 0; i < nrows; i++)
-    {
+    for (int i = 0; i < nrows; i++) {
         localB[i] = 0;
-        for (int j = 0; j < ncols_a_loc; j++)
-        {
-          localB[i] += A[i][j] * x[j];
+        for (int j = 0; j < ncols_a_loc; j++) {
+            localB[i] += A[i][j] * x[j];
         }
     }
 
@@ -35,4 +26,7 @@ void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc,
     MPI_Allgather(&ncols_a_loc, 1, MPI_INT, counts, 1, MPI_INT, MPI_COMM_WORLD);
 
     MPI_Reduce_scatter(localB, b, counts, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    free_vector(localB);
 }
+
+// srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv2 -n 311 -r 1 -c
