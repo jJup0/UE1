4c30361ea983536abe257fb91e0affc4bc18de19
Berik-Xardas <94537393+Berik-Xardas@users.noreply.github.com>
2022-06-13 11:03:12 +0200

    runtime comments

--- mpimv-student-1.0.0-Source/src/mv2.c
+++ mpimv-student-1.0.0-Source/src/mv2.c
@@ -7,6 +7,9 @@
 #include "mv.h"
 #include "utils.h"
 
+/*
+(n*m/p) + n + p
+*/
 void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc, base_t *x, int nrows_x_loc, base_t *b, int ncols_b_loc) {
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -15,6 +18,7 @@ void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc, base
     base_t *localB;
     alloc_vector(&localB, ncols);
 
+    //n*m/p
     for (int i = 0; i < nrows; i++) {
         localB[i] = 0;
         for (int j = 0; j < ncols_a_loc; j++) {
@@ -23,8 +27,10 @@ void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc, base
     }
 
     int counts[size];
+    // p + log(p)
     MPI_Allgather(&ncols_a_loc, 1, MPI_INT, counts, 1, MPI_INT, MPI_COMM_WORLD);
 
+    // n + log(p)
     MPI_Reduce_scatter(localB, b, counts, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     free_vector(localB);
 }
