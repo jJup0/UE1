2c43d8040a41a0bbce48d22f4a15d1d8b2dc7a42
Jakob R <nothanks@email.com>
2022-06-15 20:40:53 +0200

    fixed m vs. n issue in corank

--- parmerge-student-1.0.0-Source/src/merge2.c
+++ parmerge-student-1.0.0-Source/src/merge2.c
@@ -1,38 +1,34 @@
 /**
  * @file merge2.c
- * @author Elias Pinter <12023962>, Kurdo-Jaroslav Asinger <01300351>, Jakob Roithinger <52009269> 
+ * @author Elias Pinter <12023962>, Kurdo-Jaroslav Asinger <01300351>, Jakob Roithinger <52009269>
  * @brief Program merge2: This program takes a sorted array a with length n, a sorted array b with length and an array c with length n+m as input
  * and then merges the two arrays a and b into the array c by coranking.
  * @details The array C is partitioned into blocks of size (n+m)/t where t is the number of active threads. All coranks
- * for start and end of each block are computed in parallel. The coranking is performed sequentially in a bninary search like fashion. 
+ * for start and end of each block are computed in parallel. The coranking is performed sequentially in a bninary search like fashion.
  **/
 
 #include <math.h>
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "merge.h"
 
 #define SCHEDULE_STRATEGY schedule(auto)
 
-
 /**
  * @brief returns the minimum of a and b
  * @param a: int variable
- * @param b: int variable 
+ * @param b: int variable
  */
-int min(int a, int b) { 
-    return (a <= b) ? a : b; 
-}
+int min(int a, int b) { return (a <= b) ? a : b; }
 
 /**
  * @brief returns the maximum a and b
  * @param a: int variable
- * @param b: int variable 
+ * @param b: int variable
  */
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
+int max(int a, int b) { return (a > b) ? a : b; }
 
 /**
  * @brief sets the coranks corank_a and corank_b for an index i in array c
@@ -44,20 +40,20 @@ int max(int a, int b) {
  * @param m: legnth of B
  * @param corank_b: points to the address where the corank of the array B is stored
  */
-void corank(int i, double A[], long n,  int* corank_a, double B[], long m, int* corank_b) {
-    int j = min(i, m);
+void corank(int i, double A[], long n, int* corank_a, double B[], long m, int* corank_b) {
+    int j = min(i, n);
     int k = i - j;  // Because j + k = i
-    int j_low = max(0, i - n);
+    int j_low = max(0, i - m);
     int k_low = 0;  // uninit in pseudo code
     int delta;
 
     while (1) {
-        if (j > 0 && k < n && A[j - 1] > B[k]) {
+        if (j > 0 && k < m && A[j - 1] > B[k]) {
             delta = ceil((j - j_low) / 2);
             k_low = k;
             j -= delta;
             k += delta;
-        } else if (k > 0 && j < m && B[k - 1] >= A[j]) {
+        } else if (k > 0 && j < n && B[k - 1] >= A[j]) {
             delta = ceil((k - k_low) / 2);
             j_low = j;
             j += delta;
