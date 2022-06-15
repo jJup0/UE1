56bf00101f2eebc5f405ca6004373bd5723057e7
Kasinger <e1300351@student.tuwien.ac.at>
2022-06-15 16:40:09 +0200

    arrays variables noted with capitals

--- parmerge-student-1.0.0-Source/src/merge1.c
+++ parmerge-student-1.0.0-Source/src/merge1.c
@@ -6,12 +6,12 @@
 
 #define SCHEDULE_STRATEGY schedule(guided, 64)
 
-int rank(double val, double arr[], int n) {
+int rank(double val, double A[], int n) {
     int lo = 0;
     int hi = n - 1;
     while (lo <= hi) {
         int mid = (lo + hi) >> 1;
-        if (arr[mid] < val) {
+        if (A[mid] < val) {
             lo = mid + 1;
         } else {
             hi = mid - 1;
@@ -20,8 +20,8 @@ int rank(double val, double arr[], int n) {
     return lo;
 }
 
-void merge(double a[], long n, double b[], long m, double c[]) {
-#pragma omp parallel default(shared|none) shared(a, n, b, c)
+void merge(double A[], long n, double B[], long m, double C[]) {
+#pragma omp parallel default(shared|none) shared(A, n, B, C)
     {
         int i;
         int j;
@@ -29,13 +29,13 @@ void merge(double a[], long n, double b[], long m, double c[]) {
         // rank all elements in a, and insert them into c
 #pragma omp for SCHEDULE_STRATEGY nowait
         for (i = 0; i < n; i++) {
-            c[i + rank(a[i], b, m)] = a[i];
+            C[i + rank(A[i], B, m)] = A[i];
         }
 
         // rank all elements in b, and insert them into c
 #pragma omp for SCHEDULE_STRATEGY nowait
         for (j = 0; j < m; j++) {
-            c[j + rank(b[j], a, n)] = b[j];
+            C[j + rank(B[j], A, n)] = B[j];
         }
     }
 }
