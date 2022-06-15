e2f3df839d8b0742c5d237b06f271e6a5f3a552b
Jakob R <nothanks@email.com>
2022-06-15 21:51:55 +0200

    code validation script changes

--- theory_checks/code_validation.sh
+++ theory_checks/code_validation.sh
@@ -2,15 +2,19 @@
 
 cd ~/UE1/parmerge-student-1.0.0-Source
 make
-srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge1_tester -n 1234567 -m 2234567 -p 16 -c
-srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge2_tester -n 1234567 -m 2234567 -p 16 -c
-srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge3_tester -n 1234567 -m 2234567 -p 16 -c
+
+n=$((($RANDOM * $RANDOM)%1000000))
+m=$((($RANDOM * $RANDOM)%1000000))
+echo n=${n} m=${m}
+srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge1_tester -n ${n} -m ${m} -p 11 -c
+srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge2_tester -n ${n} -m $((${n} + 4298)) -p 11 -c
+srun -t 0:01 -p q_student -N 1 -c 32 ./bin/merge3_tester -n ${n} -m ${m} -p 11 -c
 
 cd ~/UE1/mpimv-student-1.0.0-Source
 module load mpi/openmpiS
 cmake .
 make
-# srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv1 -n 14123 -c
-# srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv2 -n 14123 -c
-srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv1 -n 4123 -c
-srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv2 -n 4123 -c
+n=$(($RANDOM % 4000))
+echo n=${n}
+srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv1 -n ${n} -c
+srun -p q_student -t 1 -N 3 --ntasks-per-node=7 ./bin/mv2 -n ${n} -c
